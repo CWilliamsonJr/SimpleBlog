1ed1448ed7b1122c9ee8256d0bbf5a6c2dbe405c
leo81202 <nucklesix@gmail.com>
2016-08-23 18:56:17 -0400

    Added basic login form

--- SImpleBlog/Controllers/AuthController.cs
+++ SImpleBlog/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SImpleBlog.ViewModels;
 
 namespace SImpleBlog.Controllers
 {
@@ -12,6 +13,13 @@ namespace SImpleBlog.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid) return View(form);
+            return Content("Hi " + form.Username);
         }
     }
 }
---
+++ SImpleBlog/ViewModels/Auth.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace SImpleBlog.ViewModels
+{
+    public class AuthLogin
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required,DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
